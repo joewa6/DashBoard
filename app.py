433a0627481ff03a4bb9f41a9a04850b1d0a8816
jwallace <joewallace2995@gmail.com>
2025-05-09 17:27:01 +0000

    Trigger redeploy with requirements.txt

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from sklearn.linear_model import LinearRegression
 import ccxt
 import matplotlib.pyplot as plt
 
+
 # -------------------------------
 # Helper Functions
 # -------------------------------
